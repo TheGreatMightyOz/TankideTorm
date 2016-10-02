42ff850571bbbbb38b7f6b293cdc44828fbebf0b
Jaagup Kirme <jaagupkirme@hotmail.com>
2016-10-02 17:50:58 +0300

    commit2

--- TankideTorm/src/GameBoard.java
+++ TankideTorm/src/GameBoard.java
@@ -1,8 +1,8 @@
 import java.util.ArrayList;
 
 public class GameBoard{
-	private static ArrayList<ArrayList<Integer>> board;
-	public static void setBoard(ArrayList<ArrayList<Integer>> boardOrigin) {
+	private static ArrayList<ArrayList<String>> board;
+	public static void setBoard(ArrayList<ArrayList<String>> boardOrigin) {
 		board = boardOrigin;
 	}
 }
--- TankideTorm/src/TankideTorm.java
+++ TankideTorm/src/TankideTorm.java
@@ -3,12 +3,24 @@ import java.util.ArrayList;
 public class TankideTorm {
 
 	public static void main(String[] args) throws Exception {
-		java.io.File file = new java.io.File("tankidekaart.txt");
+		System.out.println(System.getProperty("user.dir"));
+		java.io.File file = new java.io.File("TankideKaart.txt");
 		java.util.Scanner sc = new java.util.Scanner(file, "UTF-8");
-		ArrayList<ArrayList<Integer>> board = new ArrayList<ArrayList<Integer>>();
+		ArrayList<ArrayList<String>> board = new ArrayList<ArrayList<String>>();
+		String[] temp =  sc.nextLine().split(" ");
+		int boardWidth = Integer.parseInt(temp[0]);
+		int boardHeight = Integer.parseInt(temp[1]);
+		while (sc.hasNextLine()){
+			String row = sc.nextLine();
+			String[] rowList = row.split(" ");
+			ArrayList<String> rowArray = new ArrayList<String>();
+			for(String el : rowList){
+				rowArray.add(el);
+			}
+			board.add(rowArray);
+		}
 		sc.close();
 		GameBoard.setBoard(board);
-
 	}
 
 }
