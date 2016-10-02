8e0c094d54a43cf8f24f3b52b620054d525aadfc
Oskar Soop <oskasoop@gmail.com>
2016-10-02 17:39:42 +0300

    Graafika testfile

---
+++ TankideTorm/src/Graafika.java
@@ -0,0 +1,12 @@
+import java.io.IOException;
+
+public class Graafika {
+	{
+	try {
+        Runtime.getRuntime().exec(
+                        "echo hello");
+    } catch (IOException e) {
+        System.out.println("Didn't work");
+    }
+	}
+}
