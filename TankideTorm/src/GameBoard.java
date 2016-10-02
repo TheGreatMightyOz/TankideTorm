84c899e6d2149634ae286eb97e07c0cc12df9261
Jaagup Kirme <jaagupkirme@hotmail.com>
2016-10-02 18:30:16 +0300

    Meetod mängulaua muutmise jaoks

--- TankideTorm/src/GameBoard.java
+++ TankideTorm/src/GameBoard.java
@@ -1,12 +1,35 @@
 import java.util.ArrayList;
 
 public class GameBoard{
-	private static ArrayList<ArrayList<String>> board;
+	private static int isStorm = 0;
+	public static int boardWidth;
+	public static int getBoardWidth() {
+		return boardWidth;
+	}
+	public static void setBoardWidth(int boardWidth) {
+		GameBoard.boardWidth = boardWidth;
+	}
+	public static int getBoardHeight() {
+		return boardHeight;
+	}
+	public static void setBoardHeight(int boardHeight) {
+		GameBoard.boardHeight = boardHeight;
+	}
+	public static int boardHeight;
+	
+	private static ArrayList<ArrayList<Character>> board;
 	private static ArrayList<Player> playerList;
-	public static void setBoard(ArrayList<ArrayList<String>> boardOrigin) {
+	public static void setBoard(ArrayList<ArrayList<Character>> boardOrigin) {
 		board = boardOrigin;
 	}
-	public static ArrayList<ArrayList<String>> getBoard() {
+	public static ArrayList<ArrayList<Character>> getBoard() {
 		return board;
+	}
+	public static void changeMap(int[] location, Character sqType){
+		board.get(location[0]).set(location[1], sqType);
+	}
+	public static void killPlayer(Player player){
+		changeMap(player.getLocation(), '#');
+		playerList.remove(player);
 	}
 }
--- TankideTorm/src/TankideTorm.java
+++ TankideTorm/src/TankideTorm.java
@@ -6,16 +6,18 @@ public class TankideTorm {
 		System.out.println(System.getProperty("user.dir"));
 		java.io.File file = new java.io.File("TankideKaart.txt");
 		java.util.Scanner sc = new java.util.Scanner(file, "UTF-8");
-		ArrayList<ArrayList<String>> board = new ArrayList<ArrayList<String>>();
+		ArrayList<ArrayList<Character>> board = new ArrayList<ArrayList<Character>>();
 		String[] temp =  sc.nextLine().split(" ");
 		int boardWidth = Integer.parseInt(temp[0]);
 		int boardHeight = Integer.parseInt(temp[1]);
+		GameBoard.setBoardHeight(boardHeight);
+		GameBoard.setBoardWidth(boardWidth);
 		while (sc.hasNextLine()){
 			String row = sc.nextLine();
 			String[] rowList = row.split(" ");
-			ArrayList<String> rowArray = new ArrayList<String>();
+			ArrayList<Character> rowArray = new ArrayList<Character>();
 			for(String el : rowList){
-				rowArray.add(el);
+				rowArray.add(el.charAt(0));
 			}
 			board.add(rowArray);
 		}
