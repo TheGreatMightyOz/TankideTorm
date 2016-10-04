6649421495c9e37a76853a8a4e4d3f32986a9b2d
TheGreatMightyOz <oskasoop@gmail.com>
2016-10-04 21:37:11 +0300

    Create GraphicOutput.java

---
+++ TankideTorm/src/GraphicOutput.java
@@ -0,0 +1,8 @@
+
+public class GraphicOutput {
+	static void OutCharArray(char[][] graphData){
+		for(int i=0; i<graphData.length; ++i){
+			System.out.println(String.valueOf(graphData[i]));
+		}
+	}
+}
