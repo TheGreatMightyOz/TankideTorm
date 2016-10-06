b6a7e009f5fe0ce6890eef59201ef7b89745a0de
TheGreatMightyOz <oskasoop@gmail.com>
2016-10-06 20:10:23 +0300

    Update GraphicInput.java

--- TankideTorm/src/GraphicInput.java
+++ TankideTorm/src/GraphicInput.java
@@ -1,16 +1,22 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class GraphicInput {
 	public static char[][] GetCharArray(int x, int y, String filename){
 		char[][] fileData=new char[y][x];
+		for(int i=0;i<y;++i){
+			Arrays.fill(fileData[i], ' ');
+		}
+		//System.out.println(fileData[0][5]==' ');
 		//List<String> fileData = new ArrayList<String>();
 		try{
 			  Scanner file=new Scanner (new File(filename));
 			  int i=0;
 			    while(file.hasNextLine() & i<y){
 			        String line= file.nextLine();
+			        line = line + new String(new char[x-line.length()]).replace("\0", " ");
 			        //System.out.println(line);
 			        fileData[i] = line.toCharArray();
 			        ++i;
@@ -21,6 +27,7 @@ public class GraphicInput {
 			catch (IOException e){
 			    System.out.println("IOException");
 			}
+		//System.out.println(fileData[0][5]==' ');
 		return fileData;
 	}
 }
