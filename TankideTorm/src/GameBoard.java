961505709c3adc0586d39728f2a08ce0a5bc18d3
Jaagupk <Jaagupk@172.17.165.222>
2016-10-06 13:41:23 +0300

    Methods for turns. Still have to add methods for acting on player inp

--- TankideTorm/src/GameBoard.java
+++ TankideTorm/src/GameBoard.java
@@ -5,22 +5,35 @@ public class GameBoard{
 	private static int isStorm = 0;
 	private static int boardWidth;
 	public static int boardHeight;
-
+	private static char startingTileType = '$';
+	private static int splash = 1;
+	private static char filledTile = '#';
+	private static char emptyTile = '.';
+	private static int baseDamage = 1;
+	
+	
 	private static ArrayList<Player> playerList;
 	private static ArrayList<ArrayList<Character>> board;
+	private static ArrayList<ArrayList<ArrayList<Integer>>> boardTargeted = new ArrayList<ArrayList<ArrayList<Integer>>>();
+
 	
 
-	public static char getTile(int[] location){ // Returns type of tile at location 
+	public static char getTileBoard(int[] location){ // Returns type of tile at location 
 		return board.get(location[0]).get(location[1]);
 	}
+	
+	public static ArrayList<Integer> getTileBoardTargeted(int[] location){
+		return boardTargeted.get(location[0]).get(location[1]);
+	}
+	
 	
 	public static ArrayList<int[]> findStartingTiles(){ // Creates list of the locations of all starting tiles
 		ArrayList<int[]> startingTiles = new ArrayList<int[]>();
 		for(int x = 0; x < boardWidth; x++){
 			for(int y = 0; y < boardHeight; y++){
-				int[] Loc = {x,y};
-				if (GameBoard.getTile(Loc)=='1'){
-					startingTiles.add(Loc);
+				int[] location = {x,y};
+				if (GameBoard.getTileBoard(location)==startingTileType){
+					startingTiles.add(location);
 				}
 			}
 		}
@@ -31,31 +44,102 @@ public class GameBoard{
 		int size = startingTiles.size();
 		int randomIndex = (int)(Math.random()*size);
 		int[] tileLoc = startingTiles.get(randomIndex);
-		changeTile(tileLoc, '#');
+		changeTileBoard(tileLoc, filledTile);
 		return tileLoc;
 	}
 	
 	public static void genPlayerList(String[] playerNames){ // Generates locations and icons for players from the list of names given
 		for (String name: playerNames){
-			int[] Loc = GameBoard.getRandomStartingTile();
-			playerList.add(new Player(Loc, name));
+			int[] location = GameBoard.getRandomStartingTile();
+			playerList.add(new Player(location, name));
 		}
 	}
 	
 	
 	public static void spawnPlayers(){ // Spawns the players in their starting locations
 		for (Player player: playerList){
-			changeTile(player.getLocation(), player.getIcon());
+			changeTileBoard(player.getLocation(), player.getIcon());
 		}
 	}
 	
-	public static void changeTile(int[] location, Character sqType){ // Changes the tile type in location to sqType
+	public static void changeTileBoardTargeted(int[] location, int sqInt){ // 3-dimensional location is needed here
+		boardTargeted.get(location[0]).get(location[1]).set(location[2], sqInt);
+	}
+	
+	public static void changeTileBoard(int[] location, Character sqType){ // Changes the tile type in location to sqType
 		board.get(location[0]).set(location[1], sqType);
 	}
 	public static void killPlayer(Player player){ // Kills player. Leaves behind wreck.
-		changeTile(player.getLocation(), '#');
+		changeTileBoard(player.getLocation(), filledTile);
 		playerList.remove(player);
 	}
+	
+	public static void genTilesTargeted(){ // Generates a new board that shows which tiles are being targeted
+		ArrayList<ArrayList<Integer>> lineTemp = new ArrayList<ArrayList<Integer>>();
+		for (int x = 0; x < boardWidth; x++){
+			lineTemp.add(new ArrayList<Integer>());
+		}
+		for (int y = 0; y < boardHeight; y++){
+			boardTargeted.add(lineTemp);
+		}
+	}
+	
+	public static Player getPlayer(char icon){
+		for(Player player: playerList){
+			if (player.getIcon() == icon){
+				return player;
+			}
+		}
+		System.out.println("Midagi on valesti läinud. Sellist mängijat ei eksisteeri. ");  // Program shouldn't reach this far
+		return playerList.get(0);
+	}
+	
+	public static ArrayList<Player> getPlayersSurroundingTile(int[] location, int splash){
+		ArrayList<Player> playersSur = new ArrayList<Player>();
+		for (int x = location[0]-splash; x < location[0] + splash +1; x++){
+			for (int y = location[0]-splash; y < location[0] + splash +1; y++){
+				if (x < boardWidth && y < boardHeight){
+					int[] locationTemp = {x, y};
+					if (getTileBoard(locationTemp) != filledTile && getTileBoard(locationTemp) != emptyTile){
+						playersSur.add(getPlayer(getTileBoard(locationTemp)));
+					}
+				}
+				
+			}
+		}
+		return playersSur;
+	}
+	
+	public static void refreshTurn(){
+		for (int x = 0; x < boardWidth; x++){
+			for (int y = 0; y < boardHeight; y++){
+				int[] location = {x, y};
+				for (int z = 0; z < getTileBoardTargeted(location).size(); z++){
+					int[] location3d = {x, y, z};
+					int tileTargeted = getTileBoardTargeted(location).get(z);
+					if (tileTargeted > 0) {
+						changeTileBoardTargeted(location3d, tileTargeted-1);
+					} else {
+						boardTargeted.get(x).get(y).remove(z);
+					}
+				
+					if (getTileBoardTargeted(location).get(z) == 0){
+						for (Player player: getPlayersSurroundingTile(location, splash)){
+							player.takeDamage(baseDamage);
+						}
+					}
+				}
+			}
+		}
+	}
+	
+	public static void nextTurn(){
+		for(Player player: playerList){
+			player.takeTurn();
+		}
+		refreshTurn();
+	}
+	
 	public static void setIsStorm(int nr){
 		isStorm = nr;
 	}
@@ -89,5 +173,4 @@ public class GameBoard{
 	public static ArrayList<ArrayList<Character>> getBoard() {
 		return board;
 	}
-	
 }
--- TankideTorm/src/Player.java
+++ TankideTorm/src/Player.java
@@ -1,9 +1,11 @@
-
+import java.util.Scanner;
 
 public class Player {
 	private int[] location;
 	private String name;
 	private char icon; // The first letter of the name, the letter that is displayed on the map
+	private int hitpoints;
+	private static int startingHP = 3;
 	
 	public int[] getLocation() {
 		return location;
@@ -23,8 +25,30 @@ public class Player {
 	}
 	public Player(int[] location, String name) {
 		super();
+		this.hitpoints = startingHP;
 		this.location = location;
 		this.name = name;
 		this.icon = name.charAt(0);
+	}
+	public void takeDamage(int damage){
+		hitpoints -= damage;
+		if (hitpoints <= 0){
+			GameBoard.killPlayer(this);
+		}
+	}
+	public void takeTurn(){ // Method for taking a turn
+		Scanner sc = new Scanner(System.in);
+		String cmd = "";
+		while (cmd != "move" && cmd != "fire"){
+			System.out.println(name + " käik. Sisestage käsk: ");
+			cmd = sc.next().toLowerCase();
+			if (cmd.equals("move")){
+				System.out.println("Sisestage suund: "); // Lisa meetod liikumiseks
+			}
+			if (cmd.equals("fire")){ // Lisa meetod laskmiseks
+				System.out.println("Sisestage suund: ");
+			}
+		}
+		sc.close();
 	}
 }
