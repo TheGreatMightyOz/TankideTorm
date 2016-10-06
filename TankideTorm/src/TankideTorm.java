78ff0391306f486679d4841cfde763efd85a5df5
TheGreatMightyOz <oskasoop@gmail.com>
2016-10-06 20:09:28 +0300

    Update TankideTorm.java

--- TankideTorm/src/TankideTorm.java
+++ TankideTorm/src/TankideTorm.java
@@ -3,10 +3,15 @@ import java.util.ArrayList;
 public class TankideTorm {
 
 	public static void main(String[] args) throws Exception {
+		//Screen constants
+		int x=150;
+		int y=50;
 		//Sets buffer size
-		new Graafika(Integer.parseInt(args[0]),Integer.parseInt(args[1]));
+		TermCom.setScreen(x,y);
+		new Graafika(x,y);
 		//Shows welcome message
 		Graafika.welcome();
+		
 		//System.out.println(System.getProperty("user.dir"));
 		MapInput.readMap(); // Reads map from file (name input from user)
 		
