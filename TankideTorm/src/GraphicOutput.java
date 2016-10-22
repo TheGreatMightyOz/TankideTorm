957c6c1a1805b2cb8dca9b301826362399d948de
TheGreatMightyOz <oskasoop@gmail.com>
2016-10-22 16:44:28 +0300

    Update GraphicOutput.java

--- TankideTorm/src/GraphicOutput.java
+++ TankideTorm/src/GraphicOutput.java
@@ -2,7 +2,7 @@
 public class GraphicOutput {
 	static void OutCharArray(char[][] graphData){
 		for(int i=0; i<graphData.length; ++i){
-			System.out.println(String.valueOf(graphData[i]));
+			System.out.print(String.valueOf(graphData[i]));
 		}
 	}
 }
