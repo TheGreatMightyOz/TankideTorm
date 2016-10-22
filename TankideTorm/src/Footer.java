edf72479e6fe1987ca377963e162f26372cad7e0
TheGreatMightyOz <oskasoop@gmail.com>
2016-10-22 19:06:04 +0300

    Delete Footer.java

--- TankideTorm/src/Footer.java
+++
@@ -1,9 +0,0 @@
-
-public class Footer {
-	static char[][] set_footer(char[][] frame, int x,int y,String text){
-		for(int i=0; i<text.length(); ++i){
-			frame[y-1][((x-text.length())/2)+i] = text.charAt(i);
-		}
-		return frame;
-	}
-}
