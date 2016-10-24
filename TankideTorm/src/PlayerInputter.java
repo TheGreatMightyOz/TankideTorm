7bb55588d7765fd34da0080577c3df04ff0d4456
TheGreatMightyOz <oskasoop@gmail.com>
2016-10-24 17:27:17 +0300

    Update PlayerInputter.java

--- TankideTorm/src/PlayerInputter.java
+++ TankideTorm/src/PlayerInputter.java
@@ -1,14 +1,20 @@
-
-
 public class PlayerInputter { // Generates a list of player names from user input
 	public static String[] inputPlayers(){
 		java.util.Scanner sc = new java.util.Scanner(System.in);
-		System.out.println("Input the number of players: ");
+		FormatOutput.setLastFrame(FormatOutput.set_empty(150, 50));
+		GraphicOutput.OutCharArray(
+				FormatOutput.set_footer(
+						"Input the number of players:"));
+		//System.out.println("Input the number of players: ");
 		int n = Integer.parseInt(sc.nextLine());
 		String[] nameList = new String[n];
 
 		for(int i = 0; i < n; i++){
-			System.out.println("Input the name of Player " + (i+1) + ":");
+			FormatOutput.setLastFrame(FormatOutput.set_empty(150, 50));
+			GraphicOutput.OutCharArray(
+					FormatOutput.set_footer(
+							"Input the name of Player " + Integer.toString(i+1) + ":"));
+			//System.out.println("Input the name of Player " + (i+1) + ":");
 			nameList[i] = sc.nextLine();
 		}
 		
