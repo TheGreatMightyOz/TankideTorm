5517245ace46fbd824a04678c04676ba625001dd
TheGreatMightyOz <oskasoop@gmail.com>
2016-10-24 16:58:01 +0300

    Update FormatOuput.java

--- TankideTorm/src/FormatOuput.java
+++ TankideTorm/src/FormatOuput.java
@@ -1,22 +1,48 @@
+import java.util.ArrayList;
 
 public class FormatOutput {
-	static char[][] set_footer(char[][] frame, int x,int y,String text){
-		for(int i=0; i<text.length(); ++i){
-			frame[y-1][((x-text.length())/2)+i] = text.charAt(i);
-		}
-		return frame;
+	static private char[][] lastFrame;
+	
+	public static void setLastFrame(char[][] frame){
+		lastFrame=frame;
 	}
 	
-	static char[][] set_map(char[][] frame, char[][] map){
+	static char[][] set_footer(String text){
+		int x=lastFrame[0].length;
+		int y=lastFrame.length;
+		for(int i=0; i<x; ++i){
+			lastFrame[y-1][i] = ' ';
+		}
+		for(int i=0; i<text.length(); ++i){
+			lastFrame[y-1][((x-text.length())/2)+i] = text.charAt(i);
+		}
+		return lastFrame;
+	}
+	
+	static char[][] set_map(char[][] map){
+		//System.out.println(lastFrame.length);
 		for(int i=0; i<map.length; ++i){
 			for(int t=0; t<map[0].length; ++t){
-				frame[(frame.length-map.length+i)/2][t]=map[i][t];
+				lastFrame[(lastFrame.length - map.length)/2+i][(lastFrame[0].length - map[0].length)/2+t]=map[i][t];
 			}
 		}
-		return frame;
+		return lastFrame;
 	}
 	
-	static char[][] set_health(char[][] frame){
-		return frame;
+	static char[][] set_health(){
+		ArrayList<Player> playerList = GameBoard.getPlayerList();
+		for(int i=0; i<playerList.size(); ++i){
+			
+		}
+		return lastFrame;
+	}
+	static char[][] set_empty(int x,int y){
+		char[][] a = new char[y][x];
+		for(int i=0; i<y;++i){
+			for(int t=0;t<x;++t){
+				a[i][t]=' ';
+			}
+		}
+		return a;
 	}
 }
